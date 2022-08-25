acf66d97c547461131ba797f0b701d53b0aa0746
Rajesh Kalyanam <39563013+rkalyanapurdue@users.noreply.github.com>
2022-08-25 10:16:22 -0400

    Update create_processor_yml.py

--- create_processor_yml.py
+++ create_processor_yml.py
@@ -3,11 +3,11 @@
 import yaml
 import os
 
-processoror_dict = {'$1': {'SubsetAORCForcingData': 
+processor_dict = {'$1': {'SubsetAORCForcingData': 
                               {'huc12_id': os.environ['param_huc12_id'], 
                                'start_date': os.environ['param_start_date'], 
                                'end_date': os.environ['param_end_date'], 
                                'aorc_datapath':'/compute_shared/AORC_Forcing'}}}
 
 with open('/job/executable/subsetaorcforcingdata.yml','w') as procfile:
-    yaml.dump(connector_dict,procfile)
+    yaml.dump(processor_dict,procfile)
