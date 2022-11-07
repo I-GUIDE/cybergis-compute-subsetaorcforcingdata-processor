898194144504c6a7e13cf8caf75c187a5b715f28
Rajesh Kalyanam <rkalyanapurdue@gmail.com>
2022-11-07 08:47:22 -0500

    added version parameter to support AORC forcing data from AWS and Cheyenne

--- create_processor_yml.py
+++ create_processor_yml.py
@@ -7,6 +7,7 @@ processor_dict = {'$1': {'SubsetAORCForcingData':
                               {'huc12_id': os.environ['param_huc12_id'], 
                                'start_date': os.environ['param_start_date'], 
                                'end_date': os.environ['param_end_date'], 
+                               'version': os.environ['param_version'],
                                'aorc_datapath':'/compute_shared/AORC_Forcing'}}}
 
 with open('/job/executable/subsetaorcforcingdata.yml','w') as procfile:
