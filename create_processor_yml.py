654b5224d81b8c5521d4338cc583e9cdf359ac89
Rajesh Kalyanam <rkalyanapurdue@gmail.com>
2023-01-16 07:10:31 -0500

    simplified UI, made options default

--- create_processor_yml.py
+++ create_processor_yml.py
@@ -15,16 +15,16 @@ if len(shapefiles) == 1:
                                   {'shapefile': shapefiles[0],
                                    'start_date': os.environ['param_start_date'], 
                                    'end_date': os.environ['param_end_date'], 
-                                   'version': os.environ['param_version'],
-                                   'gsl': os.environ['param_gsl'],
+                                   'version': '1.0',
+                                   'gsl': 'True',
                                    'aorc_datapath':'/compute_shared/AORC_Forcing'}}}
 else:
     processor_dict = {'$1': {'SubsetAORCForcingData': 
                                   {'huc12_id': os.environ['param_huc12_id'], 
                                    'start_date': os.environ['param_start_date'], 
                                    'end_date': os.environ['param_end_date'], 
-                                   'version': os.environ['param_version'],
-                                   'gsl': os.environ['param_gsl'],
+                                   'version': '1.0',
+                                   'gsl': 'True',
                                    'aorc_datapath':'/compute_shared/AORC_Forcing'}}}
 
 with open('/job/executable/subsetaorcforcingdata.yml','w') as procfile:
