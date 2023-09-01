e00aa5607b264b0b67928319fa32541b0d9d4736
Rajesh Kalyanam <39563013+rkalyanapurdue@users.noreply.github.com>
2023-09-01 12:52:02 -0400

    Update transfer_shared_outputs.sh

--- transfer_shared_outputs.sh
+++ transfer_shared_outputs.sh
@@ -1,16 +1,7 @@
 #!/bin/bash
 
-if [[ "${param_Retrieve_Output}" = "True" ]]; then
+mkdir /compute_scratch/${job_id}
 
-  mkdir /compute_shared/${job_id}
+cd ${result_folder}
 
-  cd ${result_folder}
-
-  mv *LDASIN* /compute_shared/${job_id}/
-else
-  mkdir /compute_scratch/${job_id}
-
-  cd ${result_folder}
-
-  mv *LDASIN* /compute_scratch/${job_id}/
-fi  
+mv *LDASIN* /compute_scratch/${job_id}/
