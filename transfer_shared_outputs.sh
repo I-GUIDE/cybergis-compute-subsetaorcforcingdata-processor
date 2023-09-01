2f73baf986c0125f7de711b8a19196824f35652d
Rajesh Kalyanam <rkalyanapurdue@gmail.com>
2023-09-01 12:59:02 -0400

    modified post processing stage to not copy out result if standalone mode

--- transfer_shared_outputs.sh
+++ transfer_shared_outputs.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-mkdir /compute_scratch/${job_id}
+if [[ "${param_Retrieve_Output}" = "False" ]]; then
+  mkdir /compute_scratch/${job_id}
 
-cd ${result_folder}
+  cd ${result_folder}
 
-mv *LDASIN* /compute_scratch/${job_id}/
+  mv *LDASIN* /compute_scratch/${job_id}/
+fi
